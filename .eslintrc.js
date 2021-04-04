cd3d6477b8d24ec03116faad541bdb0199595981
Celso Alexandre <celsoalexandre@live.com>
2021-04-04 15:14:15 -0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,51 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: [
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+        ],
+      },
+    },
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'react/destructuring-assignment': 'off',
+    camelcase: 'off',
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import TaskList from './components/TaskList';
+import Header from './components/Header';
+import './styles/global.scss';
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <TaskList />
+    </>
+  );
+}
